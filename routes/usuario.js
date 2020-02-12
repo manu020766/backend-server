2b4ec55e7d5f5884b83e1e5d564b7c94a0b30c12
manu020766 <manu020766@yahoo.es>
2020-02-12 10:19:18 +0100

    add put and delete operations

--- routes/usuario.js
+++ routes/usuario.js
@@ -31,6 +31,75 @@ router.get('/', (req, res) => {
         )
 })
 
+//----------------------------------------
+//--- Actualizar un usuario --------------
+//----------------------------------------
+router.put('/:id', (req, res) => {
+
+    const id = req.params.id
+    const body = req.body
+
+    Usuario.findById(id, (err, usuario) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                mensaje: 'Error al buscar usuario',
+                errors: err
+            })
+        }
+
+        if (!usuario) {
+            return res.status(400).json({
+                ok: false,
+                mensaje: 'El usuario con el id: ' + id + ' no existe',
+                errors: { message: 'No existe un usuario con ese id' }
+            })
+        }
+
+        usuario.nombre = body.nombre
+        usuario.email = body.email
+        usuario.role = body.role
+
+        usuario.save((err, usuarioGuardado) => {
+            if (err) {
+                res.status(400).json({
+                    ok: false,
+                    mensaje: 'Error al actualizar el usuario',
+                    errors: err
+                })
+            }
+
+            usuarioGuardado.password = ':)'
+    
+            res.status(200).json({
+                ok: true,
+                usuario: usuarioGuardado
+            })
+        })
+    })   
+})
+
+//----------------------------------------
+//--- Borrar un usuario ------------------
+//----------------------------------------
+router.delete('/:id', (req, res) => {
+    const id = req.params.id
+
+    Usuario.findByIdAndDelete(id, (err, usuarioDelete) => {
+        if (err) {
+            res.status(500).json({
+                ok: false,
+                mensaje: 'No se ha podido borrar el usuario',
+                errors: err
+            })
+        }
+
+        res.status(200).json({
+            ok: true,
+            mensaje: 'Usuario borrado'
+        })
+    })
+})
 
 //----------------------------------------
 //--- Grabar un usuario ------------------
