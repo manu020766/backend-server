20fa82e5eba2fe35125defa6e8408f758c407bb8
manu020766 <manu020766@yahoo.es>
2020-02-13 11:49:40 +0100

    CRUD hospital

--- routes/hospital.js
+++ routes/hospital.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
+const { verificaToken } = require('../middlewares/autentication')
 const Hospital = require('../models/hospital')
 
 router.get('/', (req, res) => {
@@ -20,16 +21,15 @@ router.get('/', (req, res) => {
         })
 })
 
-
 //----------------------------------------
 //--- Borrar un hospital------------------
 //----------------------------------------
-router.delete('/:id', (req, res) => {
+router.delete('/:id', verificaToken, (req, res) => {
     const id = req.params.id
 
     Hospital.findByIdAndDelete(id, (err, hospitalBorrado) => {
         if (err) {
-            res.status(500).json({
+            return res.status(500).json({
                 ok: false,
                 mensaje: 'No se ha podido borrar el hospital',
                 errors: err
@@ -38,10 +38,85 @@ router.delete('/:id', (req, res) => {
 
         res.status(200).json({
             ok: true,
-            hospital: hospitalBorrado
+            hospital: hospitalBorrado,
+            usuarioToken: req.usuario
         })
     })
 })
 
+//----------------------------------------
+//--- Crear un hospital-------------------
+//----------------------------------------
+router.post('/', verificaToken, (req, res) => {
+    const body = req.body
+
+    // console.log('Usuario Token: ',req.usuario._id)
+
+    const hospital = new Hospital({
+        nombre: body.nombre,
+        image: null,
+        usuario: req.usuario._id
+    })
+    hospital.save((err, hospitalGuardardo) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                mensaje: 'No se ha podido grabar el hospital',
+                errors: err
+            })
+        }
+
+        res.status(200).json({
+            ok: true,
+            hospital: hospitalGuardardo,
+            usuarioToken: req.usuario
+        })
+    })
+})
+
+//----------------------------------------
+//--- Modificar un hospital---------------
+//----------------------------------------
+router.put('/:id', verificaToken, (req, res)=> {
+    const id = req.params.id
+    const body = req.body
+
+    Hospital.findById(id, (err, hospital)=>{
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                mensaje: 'No se ha podido encontrar el hospital',
+                errors: err
+            })
+        }
+
+        if (!hospital) {
+            return res.status(404).json({
+                ok: false,
+                mensaje: 'No se ha podido encontrar el hospital',
+                errors: err
+            })
+        }
+
+        hospital.nombre = body.nombre
+        hospital.usuario = req.usuario._id
+
+        hospital.save((err, hospitalGuardado) => {
+            if (err) {
+                res.status(400).json({
+                    ok: false,
+                    mensaje: 'Error al actualizar el hospital',
+                    errors: err
+                })
+            }
+
+            res.status(200).json({
+                ok: true,
+                hospital: hospitalGuardado,
+                usuarioToken: req.usuario
+            })
+        })
+    })
+})
 
 module.exports = router
